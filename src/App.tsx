d73b4c25d9c45ba694e31846f9c8c89355d1298d
javier <javc2804@gmail.com>
2024-05-06 20:56:07 -0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Confetti from "react-dom-confetti";
 import "./App.css";
 
 import img1 from "./assets/1.png";
-import img2 from "./assets/2.png";
 import img3 from "./assets/3.png";
 import img4 from "./assets/4.png";
 import img5 from "./assets/5.png";
@@ -63,7 +62,7 @@ const App: React.FC = () => {
   };
 
   // Imágenes de los productos para los segmentos de la ruleta
-  const products = [img1, img2, img3, img4, img5, img6];
+  const products = [img1, img3, img4, img5, img6];
 
   // Configuración del confeti
   const confettiConfig = {
